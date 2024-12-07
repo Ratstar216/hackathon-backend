bf51ddbbbab72af5ced3ad1f03f20c9247cca11e
Ratstar216 <130824137+Ratstar216@users.noreply.github.com>
2024-12-07 16:22:40 +0900

    update

--- api/main.go
+++ api/main.go
@@ -33,7 +33,7 @@ var db *sql.DB
 func init() {
 	// ①-1
 
-	err := godotenv.Load()
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("fail: godotenv.Load, %v\n", err)
 	}
@@ -170,7 +170,7 @@ func main() {
 
 	// 8000番ポートでリクエストを待ち受ける
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }

2f011c35a53f0e3f8282cea20b0f2e64960d6383
Ratstar216 <130824137+Ratstar216@users.noreply.github.com>
2024-12-07 19:05:11 +0900

    update: gettweet

--- api/main.go
+++ api/main.go
@@ -26,6 +26,22 @@ type RequestBody struct {
 	Age  int    `json:"age"`
 }
 
+type TweetGet struct {
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	Likes   int    `json:"likes"`
+	Retweet int    `json:"retweet"`
+	Content string `json:"content"`
+}
+
+type TweetPost struct {
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	Likes   int    `json:"likes"`
+	Retweet int    `json:"retweet"`
+	Content string `json:"content"`
+}
+
 // ① GoプログラムからMySQLへ接続
 var db *sql.DB
 
@@ -161,9 +177,112 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func tweethandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	switch r.Method {
+	case http.MethodGet:
+
+		// ②-1
+		// name := r.URL.Query().Get("name")
+		// if name == "" {
+		// 	log.Println("fail: name is empty")
+		// 	w.WriteHeader(http.StatusBadRequest)
+		// 	return
+		// }
+
+		// ②-2
+		rows, err := db.Query("SELECT id, name, likes, retweet, content FROM tweets")
+		if err != nil {
+			log.Printf("fail: db.Query, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// ②-3
+		tweets := make([]TweetGet, 0)
+		for rows.Next() {
+			var u TweetGet
+			if err := rows.Scan(&u.Id, &u.Name, &u.Likes, &u.Retweet, &u.Content); err != nil {
+				log.Printf("fail: rows.Scan, %v\n", err)
+
+				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
+					log.Printf("fail: rows.Close(), %v\n", err)
+				}
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			tweets = append(tweets, u)
+		}
+
+		// ②-4
+		bytes, err := json.Marshal(tweets)
+		if err != nil {
+			log.Printf("fail: json.Marshal, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bytes)
+	case http.MethodPost:
+		var requestBody TweetPost
+		err := json.NewDecoder(r.Body).Decode(&requestBody)
+		if err != nil {
+			log.Printf("fail: json.NewDecoder, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if requestBody.Name == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		tx, err := db.Begin()
+		if err != nil {
+			log.Printf("fail: db.Begin, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		ins, err := tx.Prepare("INSERT INTO user VALUES (?, ?, ?, ?, ?)")
+		if err != nil {
+			tx.Rollback()
+			log.Printf("fail: tx.Prepare, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		//id := ulid.Make().String()
+		_, err = ins.Exec(requestBody.Id, requestBody.Name, requestBody.Likes, requestBody.Retweet, requestBody.Content)
+		if err != nil {
+			tx.Rollback()
+			log.Printf("fail: ins.Exec, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if err := tx.Commit(); err != nil {
+			tx.Rollback()
+			log.Printf("fail: tx.Commit, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		// fmt.Printf("{ \"id\" : \"%s\" }", id)
+		// response := map[string]interface{}{"id": id}
+		w.Header().Set("Content-Type", "application/json")
+		// json.NewEncoder(w).Encode(response)
+	case http.MethodOptions:
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		log.Printf("fail: HTTP Method is %s\n", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
 func main() {
 	// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
 	http.HandleFunc("/user", handler)
+
+	http.HandleFunc("/tweet", tweethandler)
 
 	// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
 	closeDBWithSysCall()
